afb1b011c474c9c1493a6671719a6247358c9e28
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2023-07-10 22:09:34 -0600

    Practicing Asyncronic functions

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+function avanzaFila(callback){
+    setTimeout(function() {
+        console.log("Tu turno ha llegado");
+        callback();
+    }, 5000);
+}
+
+function mujerTeLlama(){
+    console.log("Te presentas a tu turno");
+}
+
+console.log("LLegas a la fila");
+
+avanzaFila(mujerTeLlama);
+
+console.log("Te vas a comprar cafe");
