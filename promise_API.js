7d6280a56ad446cc45040f8976a954bd8365bbe1
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2023-07-11 09:54:15 -0600

    Promise to open API

---
+++ promise_API.js
@@ -0,0 +1,23 @@
+
+function obtenerUsuarios(){
+    return new Promise(function(resolve, reject){
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+        xhr.onload = function(){
+            if(xhr.status === 200){
+                resolve(JSON.parse(xhr.responseText));
+            } else{
+                reject(xhr.statusText);
+            }
+        }
+        xhr.send();
+    });
+}
+
+obtenerUsuarios()
+.then(function(usuarios){
+    console.log(usuarios);
+})
+.catch(function(error){
+    console.log(error);
+});
