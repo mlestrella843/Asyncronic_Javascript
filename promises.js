95789ac3912780ce4c8128301a69f4e72f54e253
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2023-07-11 08:38:05 -0600

    promises

--- promises.js
+++ promises.js
@@ -4,10 +4,14 @@ let miPromesa = new Promise(function(resolve, reject){
         if(error){
             reject("La promesa ha fallado");
         }
+        else{ 
         resolve("La promesa se ha cumplido");
+        }
     }, 2000)
 })
 
 miPromesa.then(function(resultado){
     console.log(resultado);
+}).catch(function(error){
+    console.log(error);
 });
