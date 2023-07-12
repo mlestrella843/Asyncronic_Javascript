d0f5664433d4cda0121191acfe1e71c383f9084d
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2023-07-11 18:37:45 -0600

    Handling errors with promises

--- async_await_errors.js
+++ async_await_errors.js
@@ -8,20 +8,39 @@
 // sumarNumeros(1,2);
 
 //Manejo de errores con CALLBACKS
-function sumarNumeros( a, b, callback){
-    setTimeout(function(){
-        if(typeof a != 'number' || typeof b != 'number'){
-            return callback( new Error('Algun argumento no es numero'));
-        }
-        callback(null, a+b);
-    }, 1000);
+// function sumarNumeros( a, b, callback){
+//     setTimeout(function(){
+//         if(typeof a != 'number' || typeof b != 'number'){
+//             return callback( new Error('Algun argumento no es numero'));
+//         }
+//         callback(null, a+b);
+//     }, 1000);
    
-}
-//Aqui en vez de crear una funcion nombrada, se creo una funcion anonima
-sumarNumeros( 1, 2, function(error, resultado){
-    if(error){
-        console.error(error);
-    } else{
-        console.log(resultado);
+// }
+// //Aqui en vez de crear una funcion nombrada, se creo una funcion anonima
+// sumarNumeros( 1, 2, function(error, resultado){
+//     if(error){
+//         console.error(error);
+//     } else{
+//         console.log(resultado);
+//     }
+// });
+
+function sumarNumeros(a,b){
+    return new Promise(function(resolve, reject){
+        setTimeout(function(){
+            if(typeof a != 'number' || typeof b != 'number'){
+                reject(new Error("Ambos argumentos deben ser numeros"));
+            }else{
+                resolve(a+b);
+            }
+        }, 2000);
+      })       
     }
-});
+    sumarNumeros(1,2)
+    .then(function(resultado){
+        console.log(resultado);
+    }).catch(function(error){
+        console.log(error);
+    })
+
