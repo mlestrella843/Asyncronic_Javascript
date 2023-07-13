b14b4a079d76eb45158a088541c7ffd045c332f6
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2023-07-12 21:28:14 -0600

    Completing changes

--- async_await.js
+++ async_await.js
@@ -1,4 +1,7 @@
 
+//Permite manejar funciones asincronnas como si fuesen sincronas. Pues debe cumplirse cronologicamente 
+//pero evita que el servidor arroje errorres de tiempo de espera.
+
 async function obtenerDatos(){
     let respuesta = await fetch('https://api.datos.gob.mx/v1/precio.gasolina.publico');
 
